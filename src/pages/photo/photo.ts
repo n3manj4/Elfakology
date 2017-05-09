0a2365cca1aaf865ef04ba4f1d93c10a4267ef9f
Немања Миладиновић < >
2017-05-09 20:41:13 +0200

    sredjeni tabovi

--- src/pages/photo/photo.ts
+++ src/pages/photo/photo.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-photo',
+  templateUrl: 'photo.html'
 })
-export class ContactPage {
+export class PhotoPage {
 
   constructor(public navCtrl: NavController) {
 
